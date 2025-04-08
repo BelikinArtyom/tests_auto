3294b9ecaefdb2919a14be57e937f8a4db912c33
BelikinArtyom <47325838+BelikinArtyom@users.noreply.github.com>
2025-04-08 21:49:01 +0300

    Update Main.java
    !!!

--- src/main/java/tests/auto/Main.java
+++ src/main/java/tests/auto/Main.java
@@ -3,10 +3,12 @@ package tests.auto;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    public static string msg = "Hello guru!!!!";
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello guru!");
+        System.out.printf("Hello guru!!!!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
